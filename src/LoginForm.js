29bd252137b0eb1e349d32a3d606a1add676db39
Sara Heath <saramaheath@gmail.com>
2022-07-22 12:36:28 -0500

    format document

--- src/LoginForm.js
+++ src/LoginForm.js
@@ -1,20 +1,20 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 /**form to login in existing user
- * 
+ *
  * Props:
  * -login: fn called in parent
- * 
+ *
  * State:
  * -formData
  */
-function LoginForm ({login}) {
-  console.log('loginform');
-    const [formData, setFormData] = useState({})
+function LoginForm({ login }) {
+  console.log("loginform");
+  const [formData, setFormData] = useState({});
 
-    /** Update form input. */
+  /** Update form input. */
   function handleChange(evt) {
     const input = evt.target;
-    setFormData(formData => ({
+    setFormData((formData) => ({
       ...formData,
       [input.name]: input.value,
     }));
@@ -24,18 +24,26 @@ function LoginForm ({login}) {
   function handleSubmit(evt) {
     evt.preventDefault();
     login(formData);
+    console.log(formData, "formData");
   }
 
-    return(
-        <form className="LoginForm" onSubmit={handleSubmit}>
-          <span>
-            <input onChange={handleChange} name="username" placeholder="username"></input>
-            <input onChange={handleChange} name="password" placeholder="password"></input>
-            <button>log in</button>
-            </span>
-        </form>
-    )
+  return (
+    <form className="LoginForm" onSubmit={handleSubmit}>
+      <span>
+        <input
+          onChange={handleChange}
+          name="username"
+          placeholder="username"
+        ></input>
+        <input
+          onChange={handleChange}
+          name="password"
+          placeholder="password"
+        ></input>
+        <button>log in</button>
+      </span>
+    </form>
+  );
 }
-
 
 export default LoginForm;
--- src/SignupForm.js
+++ src/SignupForm.js
@@ -1,44 +1,60 @@
-import React, { useState } from "react"
+import React, { useState } from "react";
 /**form to signing up a new user
- * 
+ *
  * Props:
  * -signup: fn called in parent
- * 
+ *
  * State:
  * -formData
  */
 
-function SignupForm ({signup}) {
-    console.log("SignupForm")
-    const [formData, setFormData] = useState({})
-  
-      /** Update form input. */
-    function handleChange(evt) {
-      const input = evt.target;
-      setFormData(formData => ({
-        ...formData,
-        [input.name]: input.value,
-      }));
-    }
-  
-    /** Call parent function. */
-    function handleSubmit(evt) {
-      evt.preventDefault();
-      signup(formData);
-    }
-  
-      return(
-          <form className="SignupForm" onSubmit={handleSubmit}>
-            <span>
-              <input onChange={handleChange} name="username" placeholder="username"></input>
-              <input onChange={handleChange} name="password" placeholder="password"></input>
-              <input onChange={handleChange} name="firstName" placeholder="First Name"></input>
-              <input onChange={handleChange} name="lastName" placeholder="Last Name"></input>
-              <input onChange={handleChange} name="email" placeholder="email"></input>
-              <button>sign up</button>
-              </span>
-          </form>
-      )
+function SignupForm({ signup }) {
+  console.log("SignupForm");
+  const [formData, setFormData] = useState({});
+
+  /** Update form input. */
+  function handleChange(evt) {
+    const input = evt.target;
+    setFormData((formData) => ({
+      ...formData,
+      [input.name]: input.value,
+    }));
   }
+
+  /** Call parent function. */
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    signup(formData);
+  }
+
+  return (
+    <form className="SignupForm" onSubmit={handleSubmit}>
+      <span>
+        <input
+          onChange={handleChange}
+          name="username"
+          placeholder="username"
+        ></input>
+        <input
+          onChange={handleChange}
+          name="password"
+          placeholder="password"
+        ></input>
+        <input
+          onChange={handleChange}
+          name="firstName"
+          placeholder="First Name"
+        ></input>
+        <input
+          onChange={handleChange}
+          name="lastName"
+          placeholder="Last Name"
+        ></input>
+        <input onChange={handleChange} name="email" placeholder="email"></input>
+        <button>sign up</button>
+      </span>
+    </form>
+  );
+}
 
 export default SignupForm;
